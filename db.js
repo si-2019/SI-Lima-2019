b74b98aae55a4e5a71df67e02158aa9dc352ecd6
Džemil Džigal <ddzigal1@etf.unsa.ba>
2019-04-16 01:45:31 +0200

    dodavanje unit testova za backend

--- db.js
+++ db.js
@@ -14,7 +14,15 @@ db.sequelize = sequelize;
 db.zahtjevZaPotvrdu = sequelize.import(__dirname+"/zahtjevZaPotvrdu.js");
 db.korisnik = sequelize.import(__dirname+"/korisnik.js");
 db.svrha = sequelize.import(__dirname+"/svrhaPotvrde.js");
+db.predmet = sequelize.import(__dirname+"/predmet.js");
+
 //definisanje veza
+//1:n
 db.korisnik.hasMany(db.zahtjevZaPotvrdu,{foreignKey:'idStudenta', as:"korisnici"});
+//1:1
 db.zahtjevZaPotvrdu.belongsTo(db.svrha,{foreignKey:'idSvrhe'});
+
+//korisnik-predmet m:n
+db.korisnikPredmet = db.korisnik.belongsToMany(db.predmet,{as:'predmeti',through:'predmet_student',foreignKey:'idStudent'});
+db.predmet.belongsToMany(db.korisnik,{as:'korisnici',through:'predmet_student',foreignKey:'idPredmet'});
 module.exports=db;
--- index.js
+++ index.js
@@ -1,18 +1,105 @@
 const express = require("express");
-var cors = require('cors');
-
-const Sequelize = require('sequelize');
-const db = require(__dirname+'/db.js');
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+const db = require(__dirname + "/db.js");
 const Op = Sequelize.Op;
 
-
-db.sequelize.sync().then(function(){});
+db.sequelize.sync().then(function() {});
 const app = express();
 app.use(cors());
-app.post('/obrada',async function(req,res){
-    let ajdi =req.params.id;
-    db.zahtjevZaPotvrdu.update({obradjen:true,datumObrade:Date.now()},{where:{id:ajdi}}).then(rez=>{
-        res.end();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get("/dajSveZahtjeve", async function(req, res) {
+  let odgovor = { zahtjevi: [] };
+  db.zahtjevZaPotvrdu.findAll().then(async rez => {
+    for (let i = 0; i < rez.length; ++i) {
+      let rez1 = await db.svrha.findOne({ where: { id: rez[i].idSvrhe } });
+      let rez2 = await db.korisnik.findOne({
+        where: { id: rez[i].idStudenta }
+      });
+      let objekat = {
+        id: rez[i].id,
+        vrsta: rez1.nazivSvrhe,
+        datumZahtjeva: rez[i].datumZahtjeva,
+        oznacen: false,
+        info: {
+          idStudenta: rez2.id,
+          ime: rez2.ime,
+          prezime: rez2.prezime,
+          indeks: rez2.indeks
+        }
+      };
+      odgovor.zahtjevi.push(objekat);
+    }
+    res.json(odgovor);
+  });
+});
+app.get("/dajObradjeneZahtjeve", async function(req, res) {
+  let odgovor = { zahtjevi: [] };
+  db.zahtjevZaPotvrdu.findAll({ where: { obradjen: true } }).then(async rez => {
+    for (let i = 0; i < rez.length; ++i) {
+      let rez1 = await db.svrha.findOne({ where: { id: rez[i].idSvrhe } });
+      let rez2 = await db.korisnik.findOne({
+        where: { id: rez[i].idStudenta }
+      });
+      let objekat = {
+        id: rez[i].idZahtjev,
+        vrsta: rez1.nazivSvrhe,
+        datumZahtjeva: rez[i].datumZahtjeva,
+        oznacen: false,
+        info: {
+          idStudenta: rez2.id,
+          ime: rez2.ime,
+          prezime: rez2.prezime,
+          indeks: rez2.indeks
+        }
+      };
+      odgovor.zahtjevi.push(objekat);
+    }
+    res.json(odgovor);
+  });
+});
+app.get("/dajNeobradjeneZahtjeve", async function(req, res) {
+  let odgovor = { zahtjevi: [] };
+  db.zahtjevZaPotvrdu
+    .findAll({ where: { obradjen: false } })
+    .then(async rez => {
+      for (let i = 0; i < rez.length; ++i) {
+        let rez1 = await db.svrha.findOne({ where: { id: rez[i].idSvrhe } });
+        let rez2 = await db.korisnik.findOne({
+          where: { id: rez[i].idStudenta }
+        });
+        let objekat = {
+          id: rez[i].idZahtjev,
+          vrsta: rez1.nazivSvrhe,
+          datumZahtjeva: rez[i].datumZahtjeva,
+          oznacen: false,
+          info: {
+            idStudenta: rez2.id,
+            ime: rez2.ime,
+            prezime: rez2.prezime,
+            indeks: rez2.indeks
+          }
+        };
+        odgovor.zahtjevi.push(objekat);
+      }
+      res.json(odgovor);
     });
+});
+app.post("/obrada", async function(req, res) {
+  let ajdi = req.body.zahtjevi;
+  await ajdi.forEach(el => {
+    db.zahtjevZaPotvrdu
+      .update(
+        { obradjen: true, datumObrade: Date.now() },
+        { where: { id: el} }
+      )
+      
+  })
+
+    res.sendStatus(200);
+    res.end();
+ 
 });
 app.listen(8080);
