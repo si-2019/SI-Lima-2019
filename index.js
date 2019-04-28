149f29cc4502328f4120e39d94e4c21568298101
amesic <amesic3@etf.unsa.ba>
2019-04-28 12:31:14 +0200

    Dodan za US18 zahtjev potvrda/:index/student

--- index.js
+++ index.js
@@ -100,4 +100,37 @@ app.post("/obrada", async function(req, res) {
   res.sendStatus(200);
   res.end();
 });
+app.get("/potvrda/:index/student", async function(req, res) {
+  let odgovor = { zahtjevi: [] };
+  let ind = req.params.index;
+  let student = await db.korisnik.findOne({
+    where: { indeks: ind }
+  });
+  if (student == null) res.json(odgovor);
+  else {
+    db.zahtjevZaPotvrdu
+      .findAll({
+        where: { idStudenta: student.id }
+      })
+      .then(async rez => {
+        for (let i = 0; i < rez.length; i++) {
+          let rez1 = await db.svrha.findOne({ where: { id: rez[i].idSvrhe } });
+          let objekat = {
+            id: rez[i].id,
+            vrsta: rez1.nazivSvrhe,
+            datumZahtjeva: rez[i].datumZahtjeva,
+            stanje: rez[i].obradjen,
+            datumIzdavanja: rez[i].datumObrade,
+            besplatna: rez[i].besplatna,
+            info: {
+              ime: student.ime,
+              prezime: student.prezime
+            }
+          };
+          odgovor.zahtjevi.push(objekat);
+        }
+        res.json(odgovor);
+      });
+  }
+});
 app.listen(8080);
