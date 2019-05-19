14e28c852b8cf2d9292b7705390e674a03ad99ef
Dajana <dmojsilovi1@etf.unsa.ba>
2019-05-19 19:01:23 +0200

    svrhe i broj podnesenih zahtjeva

--- index.js
+++ index.js
@@ -133,4 +133,26 @@ app.get("/potvrda/:index/student", async function(req, res) {
       });
   }
 });
+
+app.get('/svrhe', function(req, res) {
+  db.svrha.findAll().then((result) => {
+      let jsonNiz=[];
+      for(let i=0; i<result.length; i++) {
+          let jsonObject={id: result[i].idSvrhe, naziv:result[i].nazivSvrhe};
+          jsonNiz.push(jsonObject);
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(jsonNiz));
+  });
+});
+
+app.get('/brojPodnesenih/:id', function(req, res) {
+  let jsonNiz=[];
+  db.zahtjev.count({where:{idStudenta:req.params.id}}).then(c => {
+      jsonNiz.push({idStudenta:req.params.id, brojPodnesenih: c});
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(jsonNiz));
+    });
+});
+
 app.listen(31912);
