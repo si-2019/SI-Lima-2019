d50f5ee564243eb8a736d8404af7260ad0c1f1b7
Džemil Džigal <ddzigal1@etf.unsa.ba>
2019-06-01 15:03:01 +0200

    dodavanje još dijelova rute dajPredmetPoGodini

--- izvjestaji.js
+++ izvjestaji.js
@@ -214,6 +214,64 @@ app.get("/Izvjestaji/dajPredmetPoGodini/:predmetId/:godinaId:/:filter/:datum",fu
                 });
               });
             });
+          break;
+          case "Drugi parcijalni":
+            db.ispit.findAll({where:{idPredmet:p.idPredmet,tipIspita:"Drugi parcijalni",termin:datum}}).then(async ispiti=>{
+              let nizIspitId = [];  for(let i=0;i<ispiti.length;i++) nizIspitId.push(ispiti[i].id);
+              db.ispiti_rezultati.findAll({where:{idIspit:{[op.in]:nizIspitId}}}).then(async rezultati_ispita=>{
+                // rezultati_ispita imaju podatke o studentima koji su položili kao i njihove bodove
+                db.predmetStudent.findAll({where:{idPredmet:id_predmeta}}).then(async sviStudentiNaPredmetu=>{
+                  //unutar sviKojiSuIzasli postoji ID-evi studenta, kao i akademska godina, ocjena i datum upisa ocjene za pojedini predmet.
+                    let id_studenata = []; for(let i=0;i<sviStudentiNaPredmetu.length;i++) id_studenata.push(sviStudentiNaPredmetu[i].idStudent)
+                    let ukupno = sviStudentiNaPredmetu.length;
+                    let izaslo = 0;
+                    let data = [];
+                    for(let i=0;i<rezultati_ispita.length;i++){
+                      if(id_studenata.contains(rezultati_ispita[i].idStudent)) izaslo++;
+                      if(rezultati_ispita[i].bodovi>=10) data.push(rezultati_ispita[i].bodovi);
+                    }
+                    let polozilo = data.length;
+
+                    res.json({
+                      izasloNaIspit:izaslo,
+                      ukupnoStudenata:ukupno,
+                      polozilo:polozilo,
+                      data: data
+                    });
+                });
+              });
+            });  
+          break;
+          case "Usmeni":
+              db.ispit.findAll({where:{idPredmet:p.idPredmet,tipIspita:"Usmeni",termin:datum}}).then(async ispiti=>{
+                let nizIspitId = [];  for(let i=0;i<ispiti.length;i++) nizIspitId.push(ispiti[i].id);
+                db.ispiti_rezultati.findAll({where:{idIspit:{[op.in]:nizIspitId}}}).then(async rezultati_ispita=>{
+                  // rezultati_ispita imaju podatke o studentima koji su položili kao i njihove bodove
+                  db.predmetStudent.findAll({where:{idPredmet:id_predmeta}}).then(async sviStudentiNaPredmetu=>{
+                    //unutar sviKojiSuIzasli postoji ID-evi studenta, kao i akademska godina, ocjena i datum upisa ocjene za pojedini predmet.
+                      let id_studenata = []; for(let i=0;i<sviStudentiNaPredmetu.length;i++) id_studenata.push(sviStudentiNaPredmetu[i].idStudent)
+                      let ukupno = sviStudentiNaPredmetu.length;
+                      let izaslo = 0;
+                      let data = [];
+                      for(let i=0;i<rezultati_ispita.length;i++){
+                        if(id_studenata.contains(rezultati_ispita[i].idStudent)) izaslo++;
+                        if(rezultati_ispita[i].bodovi>=10) data.push(rezultati_ispita[i].bodovi);
+                      }
+                      let polozilo = data.length;
+  
+                      res.json({
+                        izasloNaIspit:izaslo,
+                        ukupnoStudenata:ukupno,
+                        polozilo:polozilo,
+                        data: data
+                      });
+                  });
+                });
+              });  
+          break;
+          case "Prisustvo":
+          
+          break;
         default:
           res.json({message:"Greška na serveru"});
         }
